2d13ee23c9eb0c0d3b44d59574b5b2628489310e
Jacob Andrean <andreanjacob99@gmail.com>
2021-09-03 13:44:33 +0700

    Flashzilla init commit

---
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Jacob Andrean on 03/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
