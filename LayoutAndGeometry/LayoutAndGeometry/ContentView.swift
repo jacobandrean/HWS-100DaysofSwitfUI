7980a7be115c8038a3fb7f0c6eab55565e0fbdc8
Jacob Andrean <jacob.a.nityo@aux.gojek.com>
2021-09-08 13:55:24 +0700

    LayoutAndGeometry

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -19,3 +19,177 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+//MARK: -
+struct AlignmentGuideExample: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Hello, world!")
+                .alignmentGuide(.leading) { d in d[.trailing] }
+            Text("This is a longer line of text")
+            
+            ForEach(0..<10) { position in
+                Text("Number \(position)")
+                    .alignmentGuide(.leading) { _ in CGFloat(position) * -10 }
+            }
+        }
+        .background(Color.red)
+        .frame(width: 400, height: 400)
+        .background(Color.blue)
+    }
+}
+
+//MARK: -
+extension VerticalAlignment {
+    enum MidAccountAndName: AlignmentID {
+        static func defaultValue(in context: ViewDimensions) -> CGFloat {
+            context[.top]
+        }
+    }
+    
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+}
+
+struct CustomAlignmentGuideExample: View {
+    var body: some View {
+        HStack(alignment: .midAccountAndName) {
+            VStack {
+                Text("@jacobandrean")
+                    .alignmentGuide(.midAccountAndName, computeValue: { dimension in
+                        dimension[VerticalAlignment.center]
+                    })
+                Image("jacobandrean")
+                    .resizable()
+                    .frame(width: 64, height: 64, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            }
+            
+            VStack {
+                Text("Full name:")
+                Text("JACOB ANDREAN")
+                    .alignmentGuide(.midAccountAndName, computeValue: { dimension in
+                        dimension[VerticalAlignment.center]
+                    })
+                    .font(.largeTitle)
+            }
+        }
+    }
+}
+
+//MARK: -
+struct AbsolutePositionExample: View {
+    var body: some View {
+        Text("Hello, world!")
+            .position(x: 100, y: 100)
+            .background(Color.red)
+            
+    }
+}
+
+//MARK: -
+struct OuterView: View {
+    var body: some View {
+        VStack {
+            Text("Top")
+            InnerView()
+                .background(Color.green)
+            Text("Bottom")
+        }
+    }
+}
+
+struct InnerView: View {
+    var body: some View {
+        HStack {
+            Text("Left")
+            GeometryReader { geo in
+                Text("Center")
+                    .background(Color.blue)
+                    .onTapGesture {
+                        print("Global center: \(geo.frame(in: .global).midX) x \(geo.frame(in: .global).midY)")
+                        
+                        print("Custom center: \(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midY)")
+                        
+                        print("Local center: \(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midY)")
+                    }
+            }
+            .background(Color.orange)
+            Text("Right")
+        }
+    }
+}
+
+struct GeometryReaderExample2: View {
+    var body: some View {
+        OuterView()
+            .background(Color.red)
+            .coordinateSpace(name: "Custom")
+    }
+}
+
+struct GeometryReaderExample1: View {
+    var body: some View {
+        VStack {
+            GeometryReader { geo in
+                Text("Hello, world!")
+                    .frame(width: geo.size.width * 0.9, height: 40)
+                    .background(Color.red)
+            }
+            .background(Color.green)
+            
+            Text("More text")
+                .background(Color.blue)
+        }
+    }
+}
+
+//MARK:-
+struct ScrollViewWithGeometryReaderExample2: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+    
+    var body: some View {
+        GeometryReader { fullView in
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(0..<50) { index in
+                        GeometryReader { geo in
+                            Rectangle()
+                                .fill(self.colors[index % 7])
+                                .frame(height: 150)
+                                .rotation3DEffect(
+                                    .degrees(-Double(geo.frame(in: .global).midX - fullView.size.width / 2) / 10),
+                                    axis: (x: 0.0, y: 1.0, z: 0.0)
+                                )
+                        }
+                        .frame(width: 150)
+                    }
+                }
+                .padding(.horizontal, (fullView.size.width - 150) / 2)
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ScrollViewWithGeometryReaderExample1: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+    
+    var body: some View {
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { geo in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(width: fullView.size.width)
+                            .background(self.colors[index % 7])
+                            .rotation3DEffect(
+                                .degrees(Double(geo.frame(in: .global).minY - fullView.size.height / 2) / 5),
+                                axis: (x: 0.0, y: 1.0, z: 0.0)
+                            )
+                    }
+                    .frame(height: 40)
+                }
+            }
+        }
+    }
+}
